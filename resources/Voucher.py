b496b95f43695f620ca457bcdf4d4be1c47de8ad
scottcwang <wangsc@cs.wisc.edu>
2020-05-17 10:57:21 -0500

    Remove cryptography dependency from Voucher

--- resources/Voucher.py
+++ resources/Voucher.py
@@ -6,10 +6,6 @@ from datetime import datetime, timedelta
 import secrets
 import base64
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.serialization import load_der_public_key
-from cryptography.hazmat.primitives import serialization
-
 import nacl.public
 import nacl.signing
 
